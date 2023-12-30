3edf716f56a4cbdd10fe9dfd2d1c4300d54502ea
mohitvat <mohitvatyai@gmail.com>
2023-12-30 22:57:43 +0530

    favicon and title

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,4 @@
 import { Inter } from 'next/font/google'
-import Head from "next/head";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,13 +11,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <link
-          rel="icon"
-          type="image/png"
-          href="https://c.animaapp.com/iUpDz7wD/img/logo-1-2@2x.png"
-        />
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
